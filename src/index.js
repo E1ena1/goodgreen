db17c513da37997d86102ac5e6e1d710e1b22353
ElenaK <kalinakrasnaya33@gmail.com>
2023-07-23 14:05:55 +0300

    added block

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import 'normalize.css';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css'
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 import './styles/main.scss';
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+        direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+},
+
+// Navigation arrows
+// navigation: {
+//     nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+// },
+
+// And if we need scrollbar
+scrollbar: {
+    el: '.swiper-scrollbar',
+},
+});
+
+
